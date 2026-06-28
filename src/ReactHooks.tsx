ea853d8cd613d79e19b6e37f41dfa1fe019334e4
Sheraz Ali <helpfinder33@gmail.com>
2026-06-28 11:20:51 +0500

    added CountDown Timer using useEffect React Hook

--- src/ReactHooks.tsx
+++ src/ReactHooks.tsx
@@ -7,6 +7,19 @@ interface ReactHooksType {
   description: string;
 }
 
+interface ProductType {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  category: string;
+}
+
+interface CartItemType {
+  product: ProductType;
+  quantity: number;
+}
+
 const ReactHooksData: ReactHooksType[] = [
   {
     code: "1",
@@ -34,7 +47,7 @@ const ReactHooksData: ReactHooksType[] = [
   },
 ];
 
-const products = [
+const products: ProductType[] = [
   {
     id: 1,
     name: "Wireless Headphones",
@@ -70,6 +83,29 @@ const products = [
 function UseStateDemo({ hookData }: { hookData: ReactHooksType }) {
   const [counter, setCounter] = useState(0);
   const [theme, setTheme] = useState("light");
+  const [checkCart, setCheckCart] = useState(false);
+  const [cartItems, setCartItems] = useState<CartItemType[]>([]);
+
+  const handleAddToCart = (product: ProductType) => {
+    setCartItems((prevItems) => {
+      // Check if the item is already in the cart
+      const itemExists = prevItems.find(
+        (item) => item.product.id === product.id,
+      );
+
+      if (itemExists) {
+        // If it exists, increase the quantity of that specific item
+        return prevItems.map((item) =>
+          item.product.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        );
+      }
+
+      // If it's a new item, add it to the cart with a quantity of 1
+      return [...prevItems, { product, quantity: 1 }];
+    });
+  };
 
   return (
     <div>
@@ -115,42 +151,173 @@ function UseStateDemo({ hookData }: { hookData: ReactHooksType }) {
       <div className="hook-shopping-cart">
         <h2>Shopping Cart</h2>
 
-        <div className="product-display-card">
-          {products.map((product) => (
-            <div className="product-info-card">
-              <p style={{ fontSize: "60px", flexGrow: 1 }}>{product.image}</p>
+        {cartItems.length > 0 && (
+          <div className="products-cart">
+            <button
+              className="hook-button"
+              onClick={() => {
+                setCheckCart(true);
+              }}
+            >
+              Check Cart {cartItems.length}
+            </button>
+          </div>
+        )}
 
-              <p style={{ fontSize: "16px", flexGrow: 1, marginTop: "30px" }}>
-                {product.name}
-              </p>
+        {checkCart ? (
+          <div className="product-display-card">
+            {cartItems.map((cartItem) => (
+              <div className="product-info-card">
+                <p style={{ fontSize: "60px", flexGrow: 1 }}>
+                  {cartItem.product.image}
+                </p>
 
-              <p style={{ fontSize: "16px", flexGrow: 1 }}>
-                Rs. {product.price}
-              </p>
+                <p style={{ fontSize: "16px", flexGrow: 1, marginTop: "30px" }}>
+                  {cartItem.product.name}
+                </p>
 
-              <button
-                style={{ fontSize: "12px", marginTop: "10px" }}
-                className="hook-button"
-                onClick={() => {}}
-              >
-                Add To Cart
-              </button>
-            </div>
-          ))}
-        </div>
+                <p style={{ fontSize: "16px", flexGrow: 1 }}>
+                  Rs. {cartItem.product.price}
+                </p>
+
+                <button
+                  style={{ fontSize: "12px", marginTop: "10px" }}
+                  className="hook-button"
+                  onClick={() => {}}
+                >
+                  Remove Item - Quantity {cartItem.quantity}
+                </button>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <DisplayProducts addToCart={handleAddToCart} />
+        )}
       </div>
     </div>
   );
 }
 
-function useEffectDemo() {
-  return <div className="hook-demo-card"></div>;
+interface DisplayProductsProps {
+  addToCart: (product: ProductType) => void; // Callback returns nothing (void)
 }
+
+const DisplayProducts: React.FC<DisplayProductsProps> = ({ addToCart }) => {
+  return (
+    <div className="product-display-card">
+      {products.map((product) => (
+        <div className="product-info-card">
+          <p style={{ fontSize: "60px", flexGrow: 1 }}>{product.image}</p>
+
+          <p style={{ fontSize: "16px", flexGrow: 1, marginTop: "30px" }}>
+            {product.name}
+          </p>
+
+          <p style={{ fontSize: "16px", flexGrow: 1 }}>Rs. {product.price}</p>
+
+          <button
+            style={{ fontSize: "12px", marginTop: "10px" }}
+            className="hook-button"
+            onClick={() => {
+              addToCart(product);
+            }}
+          >
+            Add To Cart
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+function UseEffectDemo({ hookData }: { hookData: ReactHooksType }) {
+  return (
+    <div className="hook-demo-card">
+      <div className="use-effect-exam">
+        <h3>CountDown Timer</h3>
+        <CountDownTimer initialSeconds={3600} />
+      </div>
+      <div className="use-effect-exam">
+        <h3>Check Internet</h3>
+      </div>
+    </div>
+  );
+}
+
+interface CountDownTimerProps {
+  initialSeconds: number;
+}
+
+const CountDownTimer = ({ initialSeconds }: CountDownTimerProps) => {
+  const [seconds, setSeconds] = useState<number>(initialSeconds);
+  const [active, setActive] = useState(true);
+
+  let timer: number | null = null;
+
+  useEffect(() => {
+    if (active && seconds > 0) {
+      timer = setInterval(() => {
+        setSeconds((prev) => prev - 1);
+      }, 1000);
+    } else if (seconds === 0) {
+      setActive(false);
+    }
+
+    //clear on unmount or before running effect again
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, [active, seconds]); //Re-run effect whenever 'active or seconds' updates
+
+  //format time util function
+
+  const formatTimer = (totalSeconds: number) => {
+    const hrs = Math.floor(totalSeconds / 3600);
+    const mins = Math.floor((totalSeconds % 3600) / 60);
+    const secs = totalSeconds % 60;
+
+    // Pad single digits with a leading zero
+    return [hrs, mins, secs]
+      .map((val) => String(val).padStart(2, "0"))
+      .join(":");
+  };
+
+  return (
+    <div className="timer-box">
+      <p style={{ fontSize: "18px", fontWeight: "bold" }}>
+        {seconds > 0
+          ? "Time Remaining " + formatTimer(seconds) + "s"
+          : "⏰ Time's Up!"}
+      </p>
+
+      <div style={{ marginTop: "15px" }} className="timer-controls">
+        <button
+          className="hook-button"
+          onClick={() => {
+            setActive(!active);
+          }}
+        >
+          {active ? "Pause" : "Play"}
+        </button>
+        <button
+          className="hook-button"
+          onClick={() => {
+            (setSeconds(3600), setActive(true));
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
 
 function selectedHookView(selectedHook: ReactHooksType) {
   switch (selectedHook.name) {
     case "useState":
       return <UseStateDemo hookData={selectedHook} />;
+    case "useEffect":
+      return <UseEffectDemo hookData={selectedHook} />;
 
     default:
       return <div></div>;
@@ -158,7 +325,9 @@ function selectedHookView(selectedHook: ReactHooksType) {
 }
 
 export const ReactHooks = () => {
-  const [selectedHook, setSelectedHook] = useState<ReactHooksType | null>(null);
+  const [selectedHook, setSelectedHook] = useState<ReactHooksType>(
+    ReactHooksData[0],
+  );
   return (
     <>
       <div className="hooks-container">
